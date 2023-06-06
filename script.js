4eedeaaaf528db11a77da60cbfdae740f502c1dd
CataReygadasG <catafranrey@gmail.com>
2023-06-06 11:28:17 -0400

    Implementando js

---
+++ script.js
@@ -0,0 +1,65 @@
+const productos = [
+  {nombre: "anillo con brillante", precio:850000},
+  {nombre: "aretes dobles", precio: 350000},
+  {nombre: "collar flor", precio: 350000},
+ 
+];
+let carrito = []
+let seleccion = prompt("Bienvenido a Joyeria Sagitario, desea comprar algun producto si o no")
+while (seleccion != "si" && seleccion != "no") {
+       alert("Por favor ingrese si o no");
+       seleccion =  prompt("Desea comprar algun producto si o no");
+       break
+      }
+      if (seleccion == "si") {
+        alert("Lista de productos")
+        let todoslosProductos = productos.map(
+          (producto) => producto.nombre + " " + "$" + producto.precio 
+          );
+          alert(todoslosProductos.join(" , "));
+      }else if(seleccion == "no"){
+        alert("Gracias por visitar nuestra joyeria, hasta pronto!")
+      }
+while(seleccion != "no"){
+  let producto = prompt("Agrega un producto a tu carrito")
+  let precio = 0
+
+  if(producto == "anillo con brillante" || producto == "aretes dobles" || producto == "collar flor"){
+    switch(producto){
+      case "anillo con brillante":
+      precio = 850000;
+      break;
+      case "aretes dobles":
+        precio = 350000;
+        break;
+        case "collar flor":
+          precio =350000;
+          break;
+          
+      default:
+        break;
+    }
+    let unidades = parseInt(prompt("Cantidad: "))
+    carrito.push({producto, unidades, precio})
+    console.log(carrito)
+  }else{
+    alert("Este producto no existe")
+  }
+
+  seleccion = prompt("Desea seguir comprando?")
+  while(seleccion=== "no"){
+    alert("Gracias por su compra, hasta pronto!")
+    carrito.forEach((carritoFinal) => {
+      console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades},
+      total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
+    } )
+break
+  }
+}
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0 ) //ecc: acomulador //el: elementos
+console.log(`El total a pagar por su compra es: ${total}`)
+alert("El total a pagar por su compra es: " + total )
+
+
+
+
