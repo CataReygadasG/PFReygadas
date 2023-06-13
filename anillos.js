222ea848c0b1b51604a2f0d6bf5f91ab9ecaf397
CataReygadasG <catafranrey@gmail.com>
2023-06-13 16:38:30 -0400

    Actualizaciones

--- anillos.js
+++ anillos.js
@@ -1,1 +1,54 @@
+console.log("hola probando proyeto joyas")
+const productos = [
+    {
+        id: 1,
+        nombre: "Anillo de oro blanco con brillante",
+        precio: 890000,
+        imagen: "./img/1.jpg",
+    },
+    {
+        id: 2,
+        nombre: "Anillo líneas entrelazadas de oro blanco",
+        precio: 1000000,
+        imagen: "./img/2.jpg",
+    },
+    {
+        id: 3,
+        nombre: "Anillo de brillantes recubierto con oro blanco",
+        precio: 750000,
+        imagen: "./img/3.jpg",
+    },
+    {
+        id: 4,
+        nombre: "Argollas de compromiso de oro de 14k",
+        precio: 750000,
+        imagen: "./img/4.jpg",
+    },
+    {
+        id: 5,
+        nombre: "Anillo con diamante al medio y brillantes de plata",
+        precio: 350000,
+        imagen: "./img/5.jpg",
+    }
+]
 
+console.log(productos)
+
+let contenedorProductos = document.getElementById("contenedorProductos")
+console.log(contenedorProductos)
+productos.forEach((productoSolo)=>{
+  console.log(productoSolo.id)
+  console.log(productoSolo.nombre)
+
+
+  let contenedor = document.createElement("div")
+  contenedor.innerHTML = `
+  <article class="card1">
+  <div class="card__img" src= "${productoSolo.imagen}"></div>
+  <p class="card__descripcion">${productoSolo.nombre}</p>
+  <p class="card__descripcion">${productoSolo.precio}</p>
+  <a href="#" class="card__button">Ver más</a>
+  `
+ contenedorProductos.append(contenedor)
+
+})
