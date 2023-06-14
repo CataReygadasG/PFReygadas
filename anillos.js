3fda5f93e740d77166b4c4a6ba89787ae2aa5973
CataReygadasG <catafranrey@gmail.com>
2023-06-13 21:01:23 -0400

    Vizualizacion imagenes desde js

--- anillos.js
+++ anillos.js
@@ -4,31 +4,31 @@ const productos = [
         id: 1,
         nombre: "Anillo de oro blanco con brillante",
         precio: 890000,
-        imagen: "./img/1.jpg",
+        imagen: "../img/1.jpg",
     },
     {
         id: 2,
         nombre: "Anillo líneas entrelazadas de oro blanco",
         precio: 1000000,
-        imagen: "./img/2.jpg",
+        imagen: "../img/2.jpg",
     },
     {
         id: 3,
         nombre: "Anillo de brillantes recubierto con oro blanco",
         precio: 750000,
-        imagen: "./img/4.jpg",
+        imagen: "../img/4.jpg",
     },
     {
         id: 4,
         nombre: "Argollas de compromiso de oro de 14k",
         precio: 750000,
-        imagen: "./img/anillo1.jpg",
+        imagen: "../img/anillo1.jpg",
     },
     {
         id: 5,
         nombre: "Anillo con diamante al medio y brillantes de plata",
         precio: 350000,
-        imagen: "./img/anillo5.jpg",
+        imagen: "../img/anillo5.jpg",
     }
 ]
 
@@ -44,7 +44,7 @@ productos.forEach((productoSolo)=>{
   let contenedor = document.createElement("div")
   contenedor.innerHTML = `
   <article class="card1">
-  <div class="card__img" src="${productoSolo.imagen}" ></div>
+  <img class="card__img" src="${productoSolo.imagen}" ></img>
   <p class="card__descripcion">${productoSolo.nombre}</p>
   <p class="card__descripcion">${productoSolo.precio}</p>
   <a href="#" class="card__button">Ver más</a>
@@ -52,3 +52,5 @@ productos.forEach((productoSolo)=>{
  contenedorProductos.append(contenedor)
 
 })
+
+let miDiv = document.createElement("div")
