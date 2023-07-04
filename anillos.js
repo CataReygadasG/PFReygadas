e2051f34c00e462e4c62df5c2c3149b8896e767c
CataReygadasG <catafranrey@gmail.com>
2023-07-03 22:59:54 -0400

    catalogo de anillos y carrito funcional

--- anillos.js
+++ anillos.js
@@ -1,7 +1,7 @@
 const verCarrito = document.getElementById("verCarrito");
 const modalContainer = document.getElementById("modal-container");
 //inicializa carrito vacio
-let carritoCompras = []
+let carritoCompras = [];
 //const lista = document.getElementById('lista')
 const lista = document.getElementById("contenedorProductos");
 //console.log(lista)
@@ -23,25 +23,62 @@ fetch("../data.json")
       <button id="${productoSolo.id}" class="card__button">Comprar</button>
       `;
 			lista.append(contenedor);
-		});
-		//boton comprar
-		let botonComprar = document.getElementById(`${productoSolo.id}`);
-		//al hacer click agrega prodcutos al carrito
-		botonComprar.addEventListener("click", () => {
-			carritoCompras.push({
-				id: productoSolo.id,
-				imagen: productoSolo.imagen,
-				nombre: productoSolo.nombre,
-				precio: productoSolo.precio,
+			let botonComprar = document.getElementById(`${productoSolo.id}`);
+			//al hacer click agrega prodcutos al carrito
+			botonComprar.addEventListener("click", () => {
+				carritoCompras.push({
+					id: productoSolo.id,
+					imagen: productoSolo.imagen,
+					nombre: productoSolo.nombre,
+					precio: productoSolo.precio,
+				});
+				console.log(carritoCompras);
 			});
-			console.log(carritoCompras);
+			botonComprar.addEventListener("click", llamarSweet);
+			function llamarSweet() {
+				Swal.fire({
+					title: "Producto Agredado 🛒",
+					icon: "success",
+					confirmButtonText: "Ok",
+				});
+			}
 		});
-		botonComprar.addEventListener("click", llamarSweet);
-		function llamarSweet() {
-			Swal.fire({
-				title: "Producto Agredado 🛒",
-				icon: "success",
-				confirmButtonText: "Ok",
+		verCarrito.addEventListener("click", ()=>{
+			modalContainer.innerHTML = "";
+			modalContainer.style.display = "flex"
+			const modalHeader = document.createElement("div");
+			modalHeader.className = "modal-header";
+			modalHeader.innerHTML = `
+			<h3 class="modal-header-title">Carrito de compras</h3>
+			`;
+			modalContainer.append(modalHeader)
+			const modalbutton = document.createElement("h3");
+			modalbutton.innerText = "x"
+			modalbutton.className = "modal-header-button";
+		
+			modalbutton.addEventListener("click", () => {
+				modalContainer.style.display = "none";
+			})
+		
+			modalHeader.append(modalbutton)
+		
+			carritoCompras.forEach((productoSolo)=>{
+				let carritoContent = document.createElement("div")
+				carritoContent.className = "modal-content"
+				carritoContent.innerHTML =  `
+					<img class="imagen-carrito" src="${productoSolo.imagen}">
+					<p>${productoSolo.nombre}</p>
+					<p>${productoSolo.precio}</p>
+				`;
+				modalContainer.append(carritoContent)
 			});
-		}
+				//aac:acomulador, el: representa a cada uno de los productos
+			const total = carritoCompras.reduce((acc, el) => acc + el.precio, 0);
+		
+			const totalBuying = document.createElement("div")
+			totalBuying.className = "total-comtent"
+			totalBuying.innerHTML = `total a pagar: $ ${total} `;
+			modalContainer.append(totalBuying)
+		
+		});
 	});
