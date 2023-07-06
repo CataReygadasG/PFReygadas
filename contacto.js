dbfe433b6862efb1fc371350827c8977d22eb0a3
CataReygadasG <catafranrey@gmail.com>
2023-07-05 21:23:13 -0400

    iniciar sesion

--- contacto.js
+++ contacto.js
@@ -1,41 +1,18 @@
-let inputForm1 = document.getElementById("inputForm1")
-let inputForm2 = document.getElementById("inputForm2")
-let inputForm3 = document.getElementById("inputForm3")
-
-let botonValor = document.getElementById("botonValor")
-botonValor.addEventListener("click", mostrarValorDatos)
-
-
-function mostrarValorDatos(){
-
-    console.log("probando valores inputs")
-    console.log(inputForm1.value)
-    console.log(inputForm2.value)
-    console.log(inputForm3.value)
+let signUp = document.getElementById("signUp");
+let signIn = document.getElementById("signIn");
+let nameInput = document.getElementById("nameInput");
+let title = document.getElementById("title");
+signIn.onclick = function(){
+    nameInput.style.maxHeight = "0";
+    title.innerHTML = "Login";
+    signUp.classList.add("disable");
+    signIn.classList.remove("disable");
 
 }
+signUp.onclick = function(){
+    nameInput.style.maxHeight = "60px";
+    title.innerHTML = "Registro";
+    signUp.classList.add("disable");
+    signIn.classList.remove("disable");
 
-inputForm2.addEventListener("change",mostrarValor)
-
-function mostrarValor() {
-
-    console.log("mostrar valor")
 }
-
-
-
-
-let miFormulario = document.getElementById("formulario");
-miFormulario.addEventListener("submit", validarFormulario);
-
-function validarFormulario(e){
-    //Cancelamos el comportamiento del evento
-    e.preventDefault();
-    //Obtenemos el elemento desde el cual se disparó el evento
-    let formulario = e.target
-    //Obtengo el valor del primero hijo <input type="text">
-    console.log(formulario.children[0].value); 
-    //Obtengo el valor del segundo hijo <input type="number"> 
-    console.log(formulario.children[1].value);  
-}
-
